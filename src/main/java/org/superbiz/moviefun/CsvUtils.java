527948c82f0b59591959c5a6ad8a780dfae276d7
Raul Avila <ravila@pivotal.io>
2017-08-23 13:47:08 +0200

    Read from classpath

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -6,26 +6,12 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class CsvUtils {
-
-    public static String readFile(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
-
-            if (scanner.hasNext()) {
-                return scanner.next();
-            } else {
-                return "";
-            }
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
         try {
@@ -40,6 +26,18 @@ public class CsvUtils {
             return results;
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static String readFile(String path) {
+        InputStream inputStream = CsvUtils.class.getClassLoader().getResourceAsStream(path);
+
+        Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
+
+        if (scanner.hasNext()) {
+            return scanner.next();
+        } else {
+            return "";
         }
     }
 }
